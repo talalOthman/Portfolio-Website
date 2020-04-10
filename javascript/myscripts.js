71b39dc62a59c812db0c98ed8ff0420e45b23602
Talal <TalalOthman2000@gmail.com>
2020-04-10 13:33:21 +0800

    made the gallery option working

--- javascript/myscripts.js
+++ javascript/myscripts.js
@@ -1,3 +1,12 @@
-function showAlert(){
-    alert("Coming soon");
+const container = document.getElementById('container');
+
+document.getElementById('gallery').onclick = function(){
+    container.style.opacity = "0";
+    container.style.visibility = "hidden";
 }
+
+document.getElementById('home').onclick = function (){
+    container.style.opacity = "1";
+    container.style.visibility = "visible";
+}
+
