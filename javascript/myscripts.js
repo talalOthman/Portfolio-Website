8661d6f43aca2e877507e3da6cc12c347bc6e663
Talal <TalalOthman2000@gmail.com>
2020-04-11 12:08:44 +0800

    updated the slider

--- javascript/myscripts.js
+++ javascript/myscripts.js
@@ -17,27 +17,29 @@ document.getElementById('gallery').onclick = function(){
     swiperContainer.style.visibility = "visible";
    
 
-    var mySwiper = new Swiper ('.swiper-container', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-    
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
-    
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-    
-        // And if we need scrollbar
-        scrollbar: {
-          el: '.swiper-scrollbar',
-        },
-      })
+    var swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true,
+      },
+
+      // Navigation arrows
+  navigation: {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+  
+},
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
 
 
      
