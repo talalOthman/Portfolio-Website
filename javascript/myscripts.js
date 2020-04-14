0ab8b7b222ecb6f4394079a7efa7402c69cb30e8
Talal <TalalOthman2000@gmail.com>
2020-04-14 19:53:29 +0800

    code refactoring

--- javascript/myscripts.js
+++ javascript/myscripts.js
@@ -1,54 +1,53 @@
 const container = document.getElementById('container');
 const swiperContainer = document.getElementById('swiper-container');
-const sliderContainer = document.getElementById('slider-container');
+
 
 
 
 //when we click gallery
 document.getElementById('gallery').onclick = function(){
-    container.style.opacity = "0";
-    container.style.visibility = "hidden";
 
-    sliderContainer.style.opacity = "1";
-    sliderContainer.style.visibility = "visible";
-    
 
-    swiperContainer.style.opacity = "1";
-    swiperContainer.style.visibility = "visible";
-   
-
-    var swiper = new Swiper('.swiper-container', {
-      effect: 'coverflow',
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: 'auto',
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows : true,
-      },
-
-      // Navigation arrows
+  var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows : true,
+    },
+  
+    // Navigation arrows
   navigation: {
   nextEl: '.swiper-button-next',
   prevEl: '.swiper-button-prev',
   
-},
-      pagination: {
-        el: '.swiper-pagination',
-        dynamicBullets: true, 
-        
-      },
-    });
+  },
+    pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true, 
+      
+    },
+  });
+
+   container.style.opacity = "0";
+   container.style.visibility = "hidden";
+
+  
+    swiperContainer.style.opacity = "1";
+    swiperContainer.style.visibility = "visible";
+    
+  }
+
+  
 
 
-     
-   
-}
-
-
+ 
+  
 
 
 
@@ -56,9 +55,10 @@ document.getElementById('gallery').onclick = function(){
 document.getElementById('home').onclick = function (){
     container.style.opacity = "1";
     container.style.visibility = "visible";
+
     swiperContainer.style.opacity = "0";
     swiperContainer.style.visibility = "hidden";
-    sliderContainer.style.visibility = "hidden";
+   
     
 }
 
