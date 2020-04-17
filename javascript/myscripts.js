39ad812dd535f250eafafda0ef1b307d46c58bb2
Talal <TalalOthman2000@gmail.com>
2020-04-17 10:16:46 +0800

    changed the default event for 'a' tags

--- javascript/myscripts.js
+++ javascript/myscripts.js
@@ -1,8 +1,125 @@
-const container = document.getElementById('container');
+// HTML ELEMENTS PRACTICE
+
+let nav = document.createElement('nav');
+let ul = document.createElement('ul');
+let liHome = document.createElement('li');
+let liGallery = document.createElement('li');
+let aHome = document.createElement('a');
+let aGallery = document.createElement('a');
+
+
+
+
+
 const swiperContainer = document.getElementById('swiper-container');
 
 
+aHome.innerText = "Home";
+aGallery.innerText = "Gallery";
 
+nav.appendChild(ul);
+
+ul.appendChild(liHome);
+ul.appendChild(liGallery);
+
+liHome.appendChild(aHome);
+liGallery.appendChild(aGallery);
+
+aHome.classList.add('home');
+aGallery.classList.add('gallery');
+
+aHome.href = "#";
+aGallery.href = "#";
+
+aGallery.onclick = function(e){
+  e.preventDefault();
+  var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows : true,
+    },
+  
+    // Navigation arrows
+  navigation: {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+  
+  },
+    pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true, 
+      
+    },
+  });
+
+   div.style.opacity = "0";
+   div.style.visibility = "hidden";
+
+  
+    swiperContainer.style.opacity = "1";
+    swiperContainer.style.visibility = "visible";
+    
+  }
+
+
+  aHome.onclick = function (e){
+    e.preventDefault();
+    div.style.opacity = "1";
+    div.style.visibility = "visible";
+
+    swiperContainer.style.opacity = "0";
+    swiperContainer.style.visibility = "hidden";
+   
+}
+
+
+
+document.body.appendChild(nav);
+
+
+let div = document.createElement('div');
+div.classList.add('container');
+
+
+let aProject = document.createElement('a');
+aProject.href = "https://github.com/talalOthman?tab=repositories";
+let pProject = document.createElement('p');
+pProject.innerText = "PROJECTS";
+
+let aResume = document.createElement('a');
+aResume.href = "Resume.pdf";
+let pResume = document.createElement('p');
+pResume.innerText = "RESUME";
+
+div.appendChild(aProject);
+div.appendChild(aResume);
+
+aProject.appendChild(pProject);
+aResume.appendChild(pResume);
+
+
+document.body.appendChild(div);
+
+
+
+
+
+
+
+
+
+
+/*
+
+const swiperContainer = document.getElementById('swiper-container');
+const container = document.getElementById('container');
 
 //when we click gallery
 document.getElementById('gallery').onclick = function(){
@@ -61,4 +178,7 @@ document.getElementById('home').onclick = function (){
    
     
 }
+
+*/
+
 
