3d65507830aee696d39362a981c5491970c38cdd
Talal <TalalOthman2000@gmail.com>
2020-04-19 20:17:11 +0800

    made the footer change color when the user press 'Gallery';

--- javascript/myscripts.js
+++ javascript/myscripts.js
@@ -120,9 +120,12 @@ document.body.appendChild(div);
 
 
 
-
+const footer = document.getElementById('footer');
+const footerIcons = document.getElementsByClassName('fab');
 const swiperContainer = document.getElementById('swiper-container');
 const container = document.getElementById('container');
+
+footer.style.transition = "0.5s";
 
 //when we click gallery
 let gallery = document.getElementById('gallery');
@@ -134,6 +137,8 @@ gallery.addEventListener('click', function(e){
   sHome.style.fontWeight = "10";
   
   e.preventDefault();
+
+  footer.style.backgroundColor = "red";
 
   var swiper = new Swiper('.swiper-container', {
     effect: 'coverflow',
@@ -184,6 +189,7 @@ let sHome = document.getElementById('sHome');
 sHome.style.transition = "0.6s";
 home.addEventListener('click' , function (e){
 
+  footer.style.backgroundColor = "#3E606F";
 
   e.preventDefault();
     container.style.opacity = "1";
